95a51022d2a177b2e522ae87d575da2954f432ca
Henry Ding <henryrding@yahoo.com>
2023-03-01 00:36:31 +0000

    Added to data.js conditional to check if there is JSON data model in localStorage, if there is parse and assign to initial data variable. #2

--- js/data.js
+++ js/data.js
@@ -7,3 +7,7 @@ window.addEventListener('beforeunload', function (event) {
   var dataJSON = JSON.stringify(data);
   this.localStorage.setItem('data-local-storage', dataJSON);
 });
+
+if (localStorage.getItem('data-local-storage') !== null) {
+  data = JSON.parse(localStorage.getItem('data-local-storage'));
+}
